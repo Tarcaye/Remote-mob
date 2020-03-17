b4874f9599e1fe94d3b62d33724fb97d84b84a16
Hadrien Mens-Pellen <hmens@financeactive.com>
2020-03-17 21:11:55 +0100

    first test

--- src/test/java/io/tarcaye/remote/mob/roman/numerals/ArabicToRomanMapperTest.java
+++ src/test/java/io/tarcaye/remote/mob/roman/numerals/ArabicToRomanMapperTest.java
@@ -1,16 +1,17 @@
 package io.tarcaye.remote.mob.roman.numerals;
 
-import org.assertj.core.api.Assertions;
-import org.junit.Ignore;
 import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ArabicToRomanMapperTest {
     @Test
-    @Ignore
     public void name() {
         // 1 -> I
-        assertThat("").isEqualTo("I");
+        assertThat(getActual()).isEqualTo("I");
+    }
+
+    private String getActual() {
+        return "I";
     }
 }
