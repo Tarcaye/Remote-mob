80bc4141e15239d30a4f425612fcb7730e35f21e
Thomas Carpaye <tcarpaye@financeactive.com>
2020-03-17 21:46:52 +0100

    Refactor

--- src/test/java/io/tarcaye/remote/mob/roman/numerals/ArabicToRomanMapperTest.java
+++ src/test/java/io/tarcaye/remote/mob/roman/numerals/ArabicToRomanMapperTest.java
@@ -2,11 +2,6 @@ package io.tarcaye.remote.mob.roman.numerals;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.IntStream.range;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,27 +9,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ArabicToRomanMapperTest {
 	@Test
 	public void should_return_roman_translation_for_1() {
-        assertThat(toRoman(new Arabic(1))).isEqualTo("I");
+
+		assertThat(new Arabic(1).toRoman()).isEqualTo("I");
 	}
 
 	@Test
 	public void should_return_roman_translation_for_2() {
-		assertThat(toRoman(new Arabic(2))).isEqualTo("II");
+
+		assertThat(new Arabic(2).toRoman()).isEqualTo("II");
 	}
 
 	@Test
 	public void should_return_roman_translation_for_3() {
-		assertThat(toRoman(new Arabic(3))).isEqualTo("III");
+
+		assertThat(new Arabic(3).toRoman()).isEqualTo("III");
 	}
 
 	@Test
 	public void should_return_roman_translation_for_5() {
-		assertThat(toRoman(new Arabic(5))).isEqualTo("V");
+
+		assertThat(new Arabic(5).toRoman()).isEqualTo("V");
 	}
 
 	@Test
 	public void should_return_roman_translation_for_4() {
-		assertThat(toRoman(new Arabic(4))).isEqualTo("IV");
+
+		assertThat(new Arabic(4).toRoman()).isEqualTo("IV");
 	}
 
     public static class Arabic {
@@ -44,28 +44,15 @@ public class ArabicToRomanMapperTest {
 			this.value = value;
 		}
 
-		public int getValue() {
-			return value;
-		}
-		
-		private String toRoman(Arabic francis) {
-			if (francis.getValue() == 5) {
+		private String toRoman() {
+			if (value == 5) {
 				return "V";
 			}
-			if (francis.getValue() == 5 - 1) {
-				return toRoman(new Arabic(1)) + toRoman(new Arabic(5));
+			if (value == 5 - 1) {
+				return (new Arabic(1).toRoman()) + (new Arabic(5).toRoman());
 			}
-			return range(0, francis.getValue()).mapToObj(x -> ("I")).collect(joining());
+			return range(0, value).mapToObj(x -> ("I")).collect(joining());
 		}
 	}
 
-	private String toRoman(Arabic francis) {
-		if (francis.getValue() == 5) {
-			return "V";
-		}
-		if (francis.getValue() == 5 - 1) {
-			return toRoman(new Arabic(1)) + toRoman(new Arabic(5));
-		}
-		return range(0, francis.getValue()).mapToObj(x -> ("I")).collect(joining());
-	}
 }
