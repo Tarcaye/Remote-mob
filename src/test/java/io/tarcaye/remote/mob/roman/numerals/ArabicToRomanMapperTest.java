c2519c46fc19d645c648477813c9583f7d2ecb05
Dartz <spakpower@hotmail.fr>
2020-03-17 21:43:32 +0100

    Arabic class

--- src/test/java/io/tarcaye/remote/mob/roman/numerals/ArabicToRomanMapperTest.java
+++ src/test/java/io/tarcaye/remote/mob/roman/numerals/ArabicToRomanMapperTest.java
@@ -14,36 +14,58 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ArabicToRomanMapperTest {
 	@Test
 	public void should_return_roman_translation_for_1() {
-        assertThat(toRoman(1)).isEqualTo("I");
+        assertThat(toRoman(new Arabic(1))).isEqualTo("I");
 	}
 
 	@Test
 	public void should_return_roman_translation_for_2() {
-		assertThat(toRoman(2)).isEqualTo("II");
+		assertThat(toRoman(new Arabic(2))).isEqualTo("II");
 	}
 
 	@Test
 	public void should_return_roman_translation_for_3() {
-		assertThat(toRoman(3)).isEqualTo("III");
+		assertThat(toRoman(new Arabic(3))).isEqualTo("III");
 	}
 
 	@Test
 	public void should_return_roman_translation_for_5() {
-		assertThat(toRoman(5)).isEqualTo("V");
+		assertThat(toRoman(new Arabic(5))).isEqualTo("V");
 	}
 
 	@Test
 	public void should_return_roman_translation_for_4() {
-		assertThat(toRoman(4)).isEqualTo("IV");
+		assertThat(toRoman(new Arabic(4))).isEqualTo("IV");
 	}
 
-    private String toRoman(int i) {
-		if (i == 5) {
+    public static class Arabic {
+		private int value;
+
+		public Arabic(int value) {
+			this.value = value;
+		}
+
+		public int getValue() {
+			return value;
+		}
+		
+		private String toRoman(Arabic francis) {
+			if (francis.getValue() == 5) {
+				return "V";
+			}
+			if (francis.getValue() == 5 - 1) {
+				return toRoman(new Arabic(1)) + toRoman(new Arabic(5));
+			}
+			return range(0, francis.getValue()).mapToObj(x -> ("I")).collect(joining());
+		}
+	}
+
+	private String toRoman(Arabic francis) {
+		if (francis.getValue() == 5) {
 			return "V";
 		}
-		if (i == 5 - 1) {
-			return toRoman(1) + toRoman(5);
+		if (francis.getValue() == 5 - 1) {
+			return toRoman(new Arabic(1)) + toRoman(new Arabic(5));
 		}
-		return range(0, i).mapToObj(x -> ("I")).collect(joining());
+		return range(0, francis.getValue()).mapToObj(x -> ("I")).collect(joining());
 	}
 }
