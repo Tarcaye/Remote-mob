c31261a163d76f475e05ca1d9aea29722709b92f
Hadrien Mens-Pellen <hmens@financeactive.com>
2020-03-17 21:07:18 +0100

    Ignored first test

---
+++ src/test/java/io/tarcaye/remote/mob/roman/numerals/ArabicToRomanMapperTest.java
@@ -0,0 +1,16 @@
+package io.tarcaye.remote.mob.roman.numerals;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ArabicToRomanMapperTest {
+    @Test
+    @Ignore
+    public void name() {
+        // 1 -> I
+        assertThat("").isEqualTo("I");
+    }
+}
