4f6122db6929e86c93aa2d4222e3cf12f15eae84
Hadrien Mens-Pellen <hmens@financeactive.com>
2020-03-17 21:52:35 +0100

    Sexyness

--- src/test/java/io/tarcaye/remote/mob/roman/numerals/ArabicToRomanMapperTest.java
+++ src/test/java/io/tarcaye/remote/mob/roman/numerals/ArabicToRomanMapperTest.java
@@ -9,45 +9,63 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ArabicToRomanMapperTest {
 	@Test
 	public void should_return_roman_translation_for_1() {
-		assertThat(new Arabic(1).toRoman()).isEqualTo("I");
+		assertThat(new Arabic(1).toRoman().getValue()).isEqualTo("I");
 	}
 
 	@Test
 	public void should_return_roman_translation_for_2() {
-		assertThat(new Arabic(2).toRoman()).isEqualTo("II");
+		assertThat(new Arabic(2).toRoman().getValue()).isEqualTo("II");
 	}
 
 	@Test
 	public void should_return_roman_translation_for_3() {
-		assertThat(new Arabic(3).toRoman()).isEqualTo("III");
+		assertThat(new Arabic(3).toRoman().getValue()).isEqualTo("III");
 	}
 
 	@Test
 	public void should_return_roman_translation_for_5() {
-		assertThat(new Arabic(5).toRoman()).isEqualTo("V");
+		assertThat(new Arabic(5).toRoman().getValue()).isEqualTo("V");
 	}
 
 	@Test
 	public void should_return_roman_translation_for_4() {
-		assertThat(new Arabic(4).toRoman()).isEqualTo("IV");
+		assertThat(new Arabic(4).toRoman().getValue()).isEqualTo("IV");
 	}
 
     public static class Arabic {
-		private int value;
+		private final int value;
 
 		public Arabic(int value) {
 			this.value = value;
 		}
 
-		private String toRoman() {
+		private Roman toRoman() {
 			if (value == 5) {
-				return "V";
+				return new Roman("V");
 			}
 			if (value == 5 - 1) {
-				return new Arabic(1).toRoman() + new Arabic(5).toRoman();
+				return new Arabic(5).toRoman().substract(new Arabic(1).toRoman());
 			}
-			return range(0, value).mapToObj(x -> ("I")).collect(joining());
+			return new Roman(range(0, value).mapToObj(x -> ("I")).collect(joining()));
+		}
+
+
+	}
+	public static class Roman {
+		private final String value;
+
+		public Roman(String value) {
+			this.value = value;
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+		private Roman substract(Roman second) {
+			return new Roman(second.getValue() + getValue());
 		}
 	}
+
 
 }
