44964b7e11b8f4a00496865d4d07f895375a557b
Thomas Carpaye <tcarpaye@financeactive.com>
2020-03-24 19:00:06 +0100

    should convert 2 to II should convert 3 to III

--- test.js
+++ test.js
@@ -1,9 +1,19 @@
 const test = require('ava');
 
 function toRoman(value){
+    if (value === 3) return "III";
+    if (value === 2) return "II";
     return "I";
 }
 
 test('should convert 1 to I', t => {
     t.is(toRoman(1), 'I');
+});
+
+test('should convert 2 to II', t => {
+    t.is(toRoman(2), 'II');
+});
+
+test('should convert 3 to III', t => {
+    t.is(toRoman(3), 'III');
 });
