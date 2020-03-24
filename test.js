5d2e6743c294657f94ce4c462173efe93e3a9b77
Thomas Carpaye <tcarpaye@financeactive.com>
2020-03-24 18:40:20 +0100

    First test fail

--- test.js
+++ test.js
@@ -1,10 +1,5 @@
 const test = require('ava');
 
-test('foo', t => {
-    t.pass();
-});
-
-test('bar', async t => {
-    const bar = Promise.resolve('bar');
-    t.is(await bar, 'bar');
+test('should', async t => {
+    t.is('1', 'I');
 });
