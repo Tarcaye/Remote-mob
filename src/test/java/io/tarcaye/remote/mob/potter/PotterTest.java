5db4a2708bbac47c8cd4901105f2e760011815b5
Thomas Carpaye <tcarpaye@financeactive.com>
2020-05-18 11:14:04 +0200

    Same book ordered twice should cost 16

--- src/test/java/io/tarcaye/remote/mob/potter/PotterTest.java
+++ src/test/java/io/tarcaye/remote/mob/potter/PotterTest.java
@@ -6,20 +6,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PotterTest {
 
+    private static final int UNIT_BOOK_PRICE = 8;
+
     @Test
     public void one_book_should_cost_8() {
         // Given
-        int quantity = 1;
+        int bookQuantity = 1;
+
         // When
-        int bookPrice = calculateTotal(quantity);
+        int bookPrice = calculateTotal(bookQuantity);
+
         // Test
         assertThat(bookPrice).isEqualTo(8);
     }
 
-    private static final int UNIT_BOOK_PRICE = 8;
+    // TODO TCA: 18/05/2020 rename me
+    @Test
+    public void toto() {
+        // Given
+        int bookQuantity = 2;
 
-    private int calculateTotal(int quantity) {
-        return 0;
+        // When
+        int bookPrice = calculateTotal(bookQuantity);
+
+        // Test
+        assertThat(bookPrice).isEqualTo(16);
+    }
+
+    private int calculateTotal(int i) {
+        return i*UNIT_BOOK_PRICE;
     }
 
 }
