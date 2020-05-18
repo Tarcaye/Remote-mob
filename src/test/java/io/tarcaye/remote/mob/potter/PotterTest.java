83917aedc827a51b68b2964cc0c0611e1b4dbd6f
Imane B <50530884+imane71@users.noreply.github.com>
2020-05-18 10:48:49 +0200

    Tour 2

--- src/test/java/io/tarcaye/remote/mob/potter/PotterTest.java
+++ src/test/java/io/tarcaye/remote/mob/potter/PotterTest.java
@@ -1,10 +1,24 @@
 package io.tarcaye.remote.mob.potter;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PotterTest {
 
     @Test
     public void one_book_should_cost_8() {
+        // Given
+        int bookPrice= 8;
+        // When
+        int quantity = 1;
+        // Test
+        assertThat(calculateTotal(bookPrice, quantity)).isEqualTo(8);
+    }
 
+    private int calculateTotal(int bookPrice, int quantity) {
+        return bookPrice * quantity;
     }
 
 }
