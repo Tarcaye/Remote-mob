1780e9904c51cc2f45fff1803bcc6a8a7b720777
Thomas Carpaye <thomas.carpaye@gmail.com>
2020-05-20 09:49:02 +0200

    Move Code to Production

--- src/test/java/io/tarcaye/remote/mob/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/io/tarcaye/remote/mob/fizzbuzz/FizzBuzzTest.java
@@ -6,60 +6,47 @@ import org.junit.Test;
 public class FizzBuzzTest {
     @Test
     public void fizz_for_a_multiple_of_3() {
-        Assertions.assertThat(fizzBuzz(PositiveNumber.of(3))).isEqualTo("Fizz");
+        Assertions.assertThat(FizzBuzzNumber.of(3).fizzBuzz()).isEqualTo("Fizz");
     }
 
     @Test
     public void fizz_for_another_multiple_of_3() {
-        Assertions.assertThat(fizzBuzz(PositiveNumber.of(33))).isEqualTo("Fizz");
+        Assertions.assertThat(FizzBuzzNumber.of(33).fizzBuzz()).isEqualTo("Fizz");
     }
 
     @Test
     public void buzz_for_a_multiple_of_5() {
-        Assertions.assertThat(fizzBuzz(PositiveNumber.of(5))).isEqualTo("Buzz");
+        Assertions.assertThat(FizzBuzzNumber.of(5).fizzBuzz()).isEqualTo("Buzz");
     }
 
     @Test
     public void buzz_for_another_multiple_of_5() {
-        Assertions.assertThat(fizzBuzz(PositiveNumber.of(85))).isEqualTo("Buzz");
+        Assertions.assertThat(FizzBuzzNumber.of(85).fizzBuzz()).isEqualTo("Buzz");
     }
 
     @Test
     public void fizzbuzz_for_a_multiple_of_3_and_5() {
-        Assertions.assertThat(fizzBuzz(PositiveNumber.of(15))).isEqualTo("FizzBuzz");
+        Assertions.assertThat(FizzBuzzNumber.of(15).fizzBuzz()).isEqualTo("FizzBuzz");
     }
 
     @Test
     public void fizzbuzz_for_another_multiple_of_3_and_5() {
-        Assertions.assertThat(fizzBuzz(PositiveNumber.of(75))).isEqualTo("FizzBuzz");
+        Assertions.assertThat(FizzBuzzNumber.of(75).fizzBuzz()).isEqualTo("FizzBuzz");
     }
 
     @Test
     public void return_the_number_otherwise() {
-        Assertions.assertThat(fizzBuzz(PositiveNumber.of(4))).isEqualTo("4");
+        Assertions.assertThat(FizzBuzzNumber.of(4).fizzBuzz()).isEqualTo("4");
     }
     @Test
     public void return_the_number_otherwise_2() {
-        Assertions.assertThat(fizzBuzz(PositiveNumber.of(8))).isEqualTo("8");
+        Assertions.assertThat(FizzBuzzNumber.of(8).fizzBuzz()).isEqualTo("8");
     }
 
     @Test
     public void fail_for_a_negative_number() {
-        Assertions.assertThatThrownBy(() -> fizzBuzz(PositiveNumber.of(-1)))
+        Assertions.assertThatThrownBy(() -> FizzBuzzNumber.of(-1).fizzBuzz())
                 .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    private String fizzBuzz(PositiveNumber number) {
-        String fizzBuzz = "";
-        if (number.isAMultipleOf(3)) {
-            fizzBuzz = "Fizz";
-        }
-        if (number.isAMultipleOf(5)) {
-            fizzBuzz += "Buzz";
-        }
-        if (fizzBuzz.isEmpty())
-            return String.valueOf(number.getValue());
-        return fizzBuzz;
     }
 
 }
