4dd2831ef852e7ff3708a84a176bdd073fc22198
Thomas Carpaye <thomas.carpaye@gmail.com>
2020-05-22 14:06:44 +0200

    Rename some parametrized Test

--- src/test/java/io/tarcaye/remote/mob/fizzbuzz/ParametrizedFizzBuzzTest.java
+++ src/test/java/io/tarcaye/remote/mob/fizzbuzz/ParametrizedFizzBuzzTest.java
@@ -6,24 +6,24 @@ import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-@RunWith(JUnitParamsRunner.class)
+    @RunWith(JUnitParamsRunner.class)
 public class ParametrizedFizzBuzzTest {
 
     @Test
     @Parameters({"3", "6", "9", "99", "333"})
-    public void should_fizz_for_a(int multipleOf3) {
+    public void should_fizz_for(int multipleOf3) {
         Assertions.assertThat(FizzBuzzNumber.of(multipleOf3).fizzBuzz()).isEqualTo("Fizz");
     }
 
     @Test
     @Parameters({"5", "10", "25", "2525", "5555"})
-    public void should_buzz_for_a(int multipleOf5) {
+    public void should_buzz_for(int multipleOf5) {
         Assertions.assertThat(FizzBuzzNumber.of(multipleOf5).fizzBuzz()).isEqualTo("Buzz");
     }
 
     @Test
     @Parameters({"15", "30", "225", "555", "151515"})
-    public void should_fizzbuzz_for_a(int multipleOf3And5) {
+    public void should_fizzbuzz_for(int multipleOf3And5) {
         Assertions.assertThat(FizzBuzzNumber.of(multipleOf3And5).fizzBuzz()).isEqualTo("FizzBuzz");
     }
 
@@ -35,7 +35,7 @@ public class ParametrizedFizzBuzzTest {
 
     @Test
     @Parameters({"-4", "-8", "-44", "-88", "-7"})
-    public void fail_for_a(int negativeNumber) {
+    public void fail_for(int negativeNumber) {
         Assertions.assertThatThrownBy(() -> FizzBuzzNumber.of(negativeNumber).fizzBuzz())
                 .isInstanceOf(IllegalArgumentException.class);
     }
