ae4b9ee4cfdfeb2f446e6874abcc51a4e0c44d97
Thomas Carpaye <thomas.carpaye@gmail.com>
2020-06-11 11:04:11 +0200

    Add a golden Master

--- src/test/java/io/tarcaye/remote/mob/shodo/guilded/rose/rose/GildedRoseGoldenMaster.java
+++ src/test/java/io/tarcaye/remote/mob/shodo/guilded/rose/rose/GildedRoseGoldenMaster.java
@@ -1,10 +1,13 @@
 package io.tarcaye.remote.mob.shodo.guilded.rose.rose;
 
-public class TexttestFixture {
-    public static void main(String[] args) {
-        System.out.println("OMGHAI!");
+import org.approvaltests.Approvals;
+import org.junit.jupiter.api.Test;
 
-        Item[] items = new Item[] {
+public class GildedRoseGoldenMaster {
+
+    @Test
+    void goldenMaster() {
+        Item[] items = new Item[]{
                 new Item("+5 Dexterity Vest", 10, 20), //
                 new Item("Aged Brie", 2, 0), //
                 new Item("Elixir of the Mongoose", 5, 7), //
@@ -14,24 +17,23 @@ public class TexttestFixture {
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
                 // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
+                new Item("Conjured Mana Cake", 3, 6)};
 
         GildedRose app = new GildedRose(items);
 
-        int days = 2;
-        if (args.length > 0) {
-            days = Integer.parseInt(args[0]) + 1;
-        }
+        int days = 100;
 
+        StringBuilder result = new StringBuilder();
         for (int i = 0; i < days; i++) {
-            System.out.println("-------- day " + i + " --------");
-            System.out.println("name, sellIn, quality");
+            result.append("-------- day " + i + " --------" + "\n");
+            result.append("name, sellIn, quality" + "\n");
             for (Item item : items) {
-                System.out.println(item);
+                result.append(item + "\n");
             }
-            System.out.println();
+            result.append("\n");
             app.updateQuality();
         }
+        Approvals.verify(result.toString());
     }
 
 }
