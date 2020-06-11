4b90f005dc3b77712671e2708e5798c38726af79
Thomas Carpaye <thomas.carpaye@gmail.com>
2020-06-11 10:19:54 +0200

    Start shodo GildedRose kata

---
+++ src/test/java/io/tarcaye/remote/mob/shodo/guilded/rose/rose/GildedRoseTest.java
@@ -0,0 +1,17 @@
+package io.tarcaye.remote.mob.shodo.guilded.rose.rose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GildedRoseTest {
+
+        @Test
+        void foo() {
+            Item[] items = new Item[] { new Item("foo", 0, 0) };
+            GildedRose app = new GildedRose(items);
+            app.updateQuality();
+            assertEquals("fixme", app.items[0].name);
+        }
+
+}
