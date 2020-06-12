966df815257b5870cc52b625dea4b82d5eb71f87
Thomas Carpaye <thomas.carpaye@gmail.com>
2020-06-12 09:31:24 +0200

    Add Ugly Trivia kata

---
+++ src/test/java/io/tarcaye/remote/mob/katas/trivia/SomeTest.java
@@ -0,0 +1,13 @@
+package io.tarcaye.remote.mob.katas.trivia;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SomeTest {
+
+    @Test
+    public void true_is_true() throws Exception {
+        assertTrue(false);
+    }
+}
