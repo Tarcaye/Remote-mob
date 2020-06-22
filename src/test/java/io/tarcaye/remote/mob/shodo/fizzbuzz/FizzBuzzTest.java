1f6dc4f617ada9552553de9fe21416b322352503
mzottola <melvin.zottola@gmail.com>
2020-06-22 15:36:34 +0200

    shouldReturnFizzForMultipleOf3

--- src/test/java/io/tarcaye/remote/mob/shodo/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/io/tarcaye/remote/mob/shodo/fizzbuzz/FizzBuzzTest.java
@@ -2,6 +2,8 @@ package io.tarcaye.remote.mob.shodo.fizzbuzz;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class FizzBuzzTest {
 
@@ -15,9 +17,17 @@ public class FizzBuzzTest {
         Assertions.assertThat(converts(2)).isEqualTo("2");
     }
 
-    private String converts(final int i) {
-        return String.valueOf(i);
+    @ParameterizedTest
+    @ValueSource(ints = {6, 9, 12, 42})
+    public void shouldReturnFizzFor(final int multipleOf3) {
+        Assertions.assertThat(converts(multipleOf3)).isEqualTo("Fizz");
     }
 
+    private String converts(final int i) {
+        if (i % 3 == 0) {
+            return "Fizz";
+        }
+        return String.valueOf(i);
+    }
 
 }
