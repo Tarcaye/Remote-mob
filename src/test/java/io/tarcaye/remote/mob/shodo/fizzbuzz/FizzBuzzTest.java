2b2364232aec09e1a03a79b5341c142710b01f1b
Thomas Carpaye <thomas.carpaye@gmail.com>
2020-06-22 15:57:19 +0200

    rename converts to FizzBuzz

--- src/test/java/io/tarcaye/remote/mob/shodo/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/io/tarcaye/remote/mob/shodo/fizzbuzz/FizzBuzzTest.java
@@ -9,25 +9,52 @@ public class FizzBuzzTest {
 
     @Test
     public void shouldConvert1to1() {
-        Assertions.assertThat(converts(1)).isEqualTo("1");
+        Assertions.assertThat(fizzBuzz(1)).isEqualTo("1");
     }
 
     @Test
     public void shouldConvert2to2() {
-        Assertions.assertThat(converts(2)).isEqualTo("2");
+        Assertions.assertThat(fizzBuzz(2)).isEqualTo("2");
     }
 
     @ParameterizedTest
     @ValueSource(ints = {6, 9, 12, 42})
     public void shouldReturnFizzFor(final int multipleOf3) {
-        Assertions.assertThat(converts(multipleOf3)).isEqualTo("Fizz");
+        Assertions.assertThat(fizzBuzz(multipleOf3)).isEqualTo("Fizz");
     }
 
-    private String converts(final int i) {
-        if (i % 3 == 0) {
+    @ParameterizedTest
+    @ValueSource(ints = {10, 20, 40, 70})
+    public void shouldReturnBuzzFor(final int multipleOf5) {
+        Assertions.assertThat(fizzBuzz(multipleOf5)).isEqualTo("Buzz");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {60, 120, 90, 180})
+    public void shouldReturnFizzBuzzFor(final int multipleOf3And5) {
+        Assertions.assertThat(fizzBuzz(multipleOf3And5)).isEqualTo("FizzBuzz");
+    }
+
+    private String fizzBuzz(final int number) {
+        if (isAMultipleOf5(number) && isAMultipleOf3(number)) {
+            return "FizzBuzz";
+        }
+        if (isAMultipleOf5(number)) {
+            return "Buzz";
+        }
+        if (isAMultipleOf3(number)) {
             return "Fizz";
         }
-        return String.valueOf(i);
+        return String.valueOf(number);
     }
+
+    private boolean isAMultipleOf3(int number) {
+        return number % 3 == 0;
+    }
+
+    private boolean isAMultipleOf5(int number) {
+        return number % 5 == 0;
+    }
+
 
 }
