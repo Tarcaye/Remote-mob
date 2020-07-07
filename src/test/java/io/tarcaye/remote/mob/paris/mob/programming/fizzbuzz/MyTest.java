f759d7db87a85ba0968ba1d494911b93de8959c1
lydiane <lydiane.uhlen@hotmail.fr>
2020-07-07 21:01:48 +0200

    2eme test

--- src/test/java/io/tarcaye/remote/mob/paris/mob/programming/fizzbuzz/MyTest.java
+++ src/test/java/io/tarcaye/remote/mob/paris/mob/programming/fizzbuzz/MyTest.java
@@ -1,5 +1,7 @@
 package io.tarcaye.remote.mob.paris.mob.programming.fizzbuzz;
 
+import java.util.function.IntPredicate;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +10,16 @@ public class MyTest {
 	@Test
 	public void whenTakeOneShouldReturnOne() {
 		Assertions.assertThat(returnOne()).isEqualTo("1");
+	}
+	
+	@Test
+	public void whenTakeTwoShouldReturnTwo() {
+		Assertions.assertThat(returnOne(2)).isEqualTo("2");
+	}
+
+	private IntPredicate returnOne(int i) {
+
+		return null;
 	}
 
 	private String returnOne() {
