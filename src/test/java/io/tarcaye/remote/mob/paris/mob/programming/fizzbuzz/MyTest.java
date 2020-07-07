120ebdae3d954fc43d10b29ac059a73ab34bd677
lydiane <lydiane.uhlen@hotmail.fr>
2020-07-07 20:35:29 +0200

    premier test

--- src/test/java/io/tarcaye/remote/mob/paris/mob/programming/fizzbuzz/MyTest.java
+++ src/test/java/io/tarcaye/remote/mob/paris/mob/programming/fizzbuzz/MyTest.java
@@ -1,4 +1,12 @@
 package io.tarcaye.remote.mob.paris.mob.programming.fizzbuzz;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MyTest {
+	
+	@Test
+	public void whenTakeOneReturnOne() {
+		Assertions.assertThat("").isEqualTo("1");
+	}
 }
